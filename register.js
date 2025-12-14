c2246a7afb74135151a489a8530308dd435a45fd
anita065 <af73858@gmail.com>
2025-12-14 18:23:15 +0100

    shtova validim per fullname ne register form

--- register.js
+++ register.js
@@ -4,11 +4,15 @@ registerForm.addEventListener("submit", function(e){
     e.preventDefault(); 
 
     const fullname = document.getElementById("fullname").value.trim();
-
+    const username = document.getElementById("username").value.trim();
     if(fullname.length < 3){
         alert("Full Name duhet të ketë minimum 3 karaktere.");
         return;
     }
 
-    alert("Fullname valid!");
+    if(username.length === 0){
+        alert("Username duhet të plotësohet.");
+        return;
+    }
+    alert("Fullname dhe username valid!");
 });
