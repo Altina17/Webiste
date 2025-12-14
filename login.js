c6c7a8ac7d3b492253fd55c152cb392a7c6d7ff4
anita065 <af73858@gmail.com>
2025-12-14 20:02:38 +0100

    Shto login.js dhe test lidhje me login form

---
+++ login.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function(e){
+    e.preventDefault(); 
+    alert("Form po funksionon, JS i lidhur!");
+});
